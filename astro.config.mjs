fae26ecbd16d638a24b5571426f8650f2849e96d
magicroot75 <jpmaguire2@gmail.com>
2026-05-19 15:21:59 -0400

    Add 301 redirects for old WordPress date-style URLs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,10 @@ import { defineConfig, fontProviders } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://jackmaguire.org',
+	redirects: {
+		'/2026/01/24/my-brother-is-building-a-data-platform-for-fishermen-and-its-exposing-how-broken-the-industry-really-is/': '/blog/my-brother-building-data-platform-fishermen',
+		'/2026/01/10/william-gaddis-recognitions-my-thoughts/': '/blog/william-gaddis-recognitions',
+	},
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
