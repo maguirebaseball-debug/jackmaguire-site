017ba380b9d576530dde4eff610189a1abd8fdf2
Jack Maguire <maguirebaseball@gmail.com>
2026-08-08 18:27:15 -0400

    Load GA4 bootstrap as classic script

---
+++ public/ga4-bootstrap.js
@@ -0,0 +1,6 @@
+window.dataLayer = window.dataLayer || [];
+window.gtag = window.gtag || function () {
+	window.dataLayer.push(arguments);
+};
+window.gtag('js', new Date());
+window.gtag('config', 'G-1697T7D92W');
